e60ce99302beea8c0d56527db3eed3eb4a40c09c
tariqq10 <Kwilliam866@gmail.com>
2024-11-11 19:59:02 +0300

    admin request features

--- client/src/Admin/pages/AdminDashboard.jsx
+++ client/src/Admin/pages/AdminDashboard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import DashboardOverview from '../components/Dashboard/DashboardOverview';
+import '../styles/dashboard.css';
 
 const AdminDashboard = () => {
   return (
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,10 @@
 import React from 'react';
-import './Admin/styles/dashboard.css';
-
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import AdminDashboard from './Admin/pages/AdminDashboard'; // Corrected to include the `Admin` folder
+import AppRouter from './Routes/router';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/admin" element={<AdminDashboard />} />
-      <Route path="/" element={<Navigate to="/admin" replace />} />
-    </Routes>
-  </Router>
-);
+function App() {
+  return <AppRouter />;
+}
 
 export default App;
-
-
-
