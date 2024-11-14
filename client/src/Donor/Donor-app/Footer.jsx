dbf025b67b9e02b5af6f72906320cdde58f46e8b
Curtis-png <curtis.nganga@student.moringaschool.com>
2024-11-14 21:32:50 +0300

    DonorPages

---
+++ client/src/Donor/Donor-app/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./assets/styles/NavBar.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 Donation Platform. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
